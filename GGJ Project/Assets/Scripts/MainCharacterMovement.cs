9796aa9b4d417aa3a73d2f97cce8f769a03be67d
The355 <98568002+The355@users.noreply.github.com>
2022-01-29 20:48:10 +0700

    Revert "Merge branch 'main' of https://github.com/Axylis/GGJ-Project"
    This reverts commit 00ef549c44a0537c5a1876a49fe4e80914633b42, reversing
    changes made to a08e2d0a111af45017a8b938e9f61d5ede61798f.

--- GGJ Project/Assets/Scripts/MainCharacterMovement.cs
+++ GGJ Project/Assets/Scripts/MainCharacterMovement.cs
@@ -2,41 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
 public class MainCharacterMovement : MonoBehaviour
 {
-    public float moveSpeed = 10f;
+    public float moveSpeed = 1f;
+    public List<Vector2> depthLevel;
+    public Vector2 horizontalLimit;
     private float horizontalMovement = 0f;
     private SpriteRenderer sprite;
-    public Animation spriteanim;
-    public Animator animcontrol;
-    // Update is called once per frame
+    private Animator spriteanim;
+
     private void Start()
     {
-        DontDestroyOnLoad(this);
         sprite = GetComponent<SpriteRenderer>();
-        if(sprite == null)
-        {
-            Debug.LogError("Sprite Missing");
-        }
+        spriteanim = GetComponent<Animator>();
     }
     private void Update()
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(horizontalMovement, 0, 0) * Time.deltaTime * moveSpeed;
-        Debug.Log("(" + transform.position.x + "," + transform.position.y + ")");
-        if(Input.GetAxisRaw("Horizontal") < 0)
-        {
-            sprite.flipX = false;
-        }
-        else if(Input.GetAxisRaw("Horizontal") > 0)
-        {
-            sprite.flipX = true;
+        Move();
+        Animate();
+    }
+
+    private void Move(){
+        float horizontalPos = transform.position.x + horizontalMovement * Time.deltaTime * moveSpeed;
+        if(horizontalPos < horizontalLimit[0]){
+            horizontalPos = horizontalLimit[0];
+        }else if(horizontalPos > horizontalLimit[1]){
+            horizontalPos = horizontalLimit[1];
         }
 
+        float verticalPos = transform.position.y;
+        if(depthLevel.Count > 1){
+            for (int i = 0; i < depthLevel.Count; i++){
+                if(depthLevel[i].x < transform.position.x){
+                    verticalPos = depthLevel[i].y;
+                }
+            }
+        }
+        
+        transform.position = new Vector3(horizontalPos, verticalPos, transform.position.z);
+    }
 
-        if(Input.GetAxisRaw("Horizontal") == 0)
-        {
-            spriteanim.Stop();
+    private void Animate(){
+        if(horizontalMovement != 0){
+            sprite.flipX = horizontalMovement > 0;
+            spriteanim.Play("Walk");
+        }else{
+            spriteanim.Play("Idle");
         }
     }
 }
