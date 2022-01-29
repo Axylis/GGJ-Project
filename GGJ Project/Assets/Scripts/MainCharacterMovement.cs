8d99e5f1df80881f839a8f7aea7e93f9e3716eb2
The355 <98568002+The355@users.noreply.github.com>
2022-01-29 19:39:25 +0700

    Improved Character Movement
    Added horizontal bounding limit
    Added vertical interaction
    Changed Idle Motion
    Reduced Speed

--- GGJ Project/Assets/Scripts/MainCharacterMovement.cs
+++ GGJ Project/Assets/Scripts/MainCharacterMovement.cs
@@ -3,37 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
 public class MainCharacterMovement : MonoBehaviour
 {
-    public float moveSpeed = 10f;
+    public float moveSpeed = 1f;
+    public List<Vector2> depthLevel;
+    public Vector2 horizontalLimit;
     private float horizontalMovement = 0f;
     private SpriteRenderer sprite;
-    public Animation spriteanim;
-    // Update is called once per frame
+    private Animator spriteanim;
+
     private void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
-        if(sprite == null)
-        {
-            Debug.LogError("Sprite Missing");
-        }
+        spriteanim = GetComponent<Animator>();
     }
     private void Update()
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(horizontalMovement, 0, 0) * Time.deltaTime * moveSpeed;
-        Debug.Log("(" + transform.position.x + "," + transform.position.y + ")");
-        if(Input.GetAxisRaw("Horizontal") < 0)
-        {
-            sprite.flipX = false;
+        Move();
+        Animate();
+    }
+
+    private void Move(){
+        float horizontalPos = transform.position.x + horizontalMovement * Time.deltaTime * moveSpeed;
+        if(horizontalPos < horizontalLimit[0]){
+            horizontalPos = horizontalLimit[0];
+        }else if(horizontalPos > horizontalLimit[1]){
+            horizontalPos = horizontalLimit[1];
         }
-        else if(Input.GetAxisRaw("Horizontal") > 0)
-        {
-            sprite.flipX = true;
+
+        float verticalPos = transform.position.y;
+        if(depthLevel.Count > 1){
+            for (int i = 0; i < depthLevel.Count; i++){
+                if(depthLevel[i].x < transform.position.x){
+                    verticalPos = depthLevel[i].y;
+                }
+            }
         }
-        else if(Input.anyKey == false)
-        {
-            spriteanim.Stop();
+        
+        transform.position = new Vector3(horizontalPos, verticalPos, transform.position.z);
+    }
+
+    private void Animate(){
+        if(horizontalMovement != 0){
+            sprite.flipX = horizontalMovement > 0;
+            spriteanim.Play("Walk");
+        }else{
+            spriteanim.Play("Idle");
         }
     }
 }
