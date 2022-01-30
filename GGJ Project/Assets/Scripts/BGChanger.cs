4d48403d34af955ff20d0cfd8133e93986ba8cd9
Axylis <78213578+Axylis@users.noreply.github.com>
2022-01-30 14:11:44 +0700

    bg change (unfinished)

--- GGJ Project/Assets/Scripts/BGChanger.cs
+++ GGJ Project/Assets/Scripts/BGChanger.cs
@@ -1,24 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.U2D.Animation;
 
+[RequireComponent(typeof(Collider2D))]
 public class BGChanger : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
+    public SpriteRenderer bgRenderer;
     public Sprite[] bg;
-    public Sprite[] player;
+    public SpriteRenderer playerRenderer;
     private int i;
+    private List<SpriteResolver> _resolvers;
 
-    public void changeSprite()
+    private void Awake()
+    {
+        //_resolvers = GetComponentsInChildren<SpriteResolver>().ToList();
+    }
+
+    public void OnMouseDown()
+    {
+        BGChange();
+    }
+
+    private void BGChange()
     {
         if(i == 0)
         {
-            spriteRenderer.sprite = bg[i+1];
+            bgRenderer.sprite = bg[i+1];
             i++;
         }
         else
         {
-            spriteRenderer.sprite = bg[0];
+            bgRenderer.sprite = bg[0];
             i--;
         }
     }
